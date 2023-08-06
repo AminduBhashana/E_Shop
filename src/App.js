d1f0accf94ad0f979dfd87f474e44190d488d638
AminduBhashana <waamidu@gmail.com>
2023-08-07 01:21:20 +0530

    add route syatem

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Home from './modules/Home';
+import Footer from './components/Footer';
+import { Routes ,Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <Header/>
-      <Home/>
+      <Routes>
+        <Route path = "/" element = {<Home/>}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -27,7 +27,6 @@ const Home = () => {
         }
         <Products/>
         <FeatureCard/>
-        <Footer/>
       </>
     )
 }
