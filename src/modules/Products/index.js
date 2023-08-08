bd1fbfa0e43fb578f6c9e7e4b5f933a124384dbe
AminduBhashana <waamidu@gmail.com>
2023-08-08 19:57:02 +0530

    create pagination and filters

--- src/modules/Products/index.js
+++ src/modules/Products/index.js
@@ -7,6 +7,7 @@ const Products = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [productsPerPage] = useState(20);
     const [totalProducts, setTotalProducts] = useState(0);
+    const [searchQuery, setSearchQuery] = useState('');
 
     useEffect(() => {
        const fetchProducts = async () => {
@@ -22,6 +23,18 @@ const Products = () => {
 
     const pageNumbers = Array.from({ length: totalPages }, (_, index) => index + 1);
 
+    const handleSearch = async () => {
+      if (searchQuery.trim() === '') {
+          return;
+      }
+
+      const response = await fetch(`https://dummyjson.com/products/search?q=${searchQuery}`);
+      const data = await response.json();
+      setProducts(data.products);
+      setTotalProducts(data.total);
+      setCurrentPage(1); 
+  };
+
     return (
         <div>
           <Categories />
@@ -29,6 +42,21 @@ const Products = () => {
             <h2 className="text-xs text-blue-500 tracking-widest font-medium title-font mb-1">PRODUCTS</h2>
             <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">ALL PRODUCTS</h1>
           </div>
+          <div className="flex items-center justify-center mt-4">
+                <input
+                    type="text"
+                    placeholder="Search products"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    className="w-64 px-4 py-2 border rounded-l-lg focus:outline-none"
+                />
+                <button
+                    onClick={handleSearch}
+                    className="px-4 py-2 bg-blue-500 text-white rounded-r-lg hover:bg-blue-600 focus:outline-none"
+                >
+                    Search
+                </button>
+            </div>
           {products.length > 0 ? (
             <>
               <ProductCard products={products} />
